4c348d994fe821e27a071351f1edca74be70d095
Milosz B <miloszbargiel@gmail.com>
2022-09-28 13:30:43 +0200

    Remove unnecessary import

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,6 @@
 import UsersRepository from '../repositories/users-repository';
 import IUser from '../interfaces/user.interface';
 import BaseController from './base-controller';
-import { IfExists } from '../interfaces/generic-helpers';
 
 class UsersController extends BaseController<IUser> {
   constructor(private usersRepository: UsersRepository) {
