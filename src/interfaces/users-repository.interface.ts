2c0dcc44c134fd6a08ab329fd24841c6548bd7b2
Milosz B <miloszbargiel@gmail.com>
2022-09-28 16:42:43 +0200

    Improve UsersRepository type definition

--- src/interfaces/users-repository.interface.ts
+++ src/interfaces/users-repository.interface.ts
@@ -1,8 +1,8 @@
-import IUser from './user.interface';
+import { SerializedUser, IUser } from './user.interface';
 import IBaseRepository from './base-repository.interface';
 
-interface IUserRepository extends IBaseRepository<IUser> {
-  getUsersByRole(role: string): IUser[];
+interface IUserRepository extends IBaseRepository<IUser, SerializedUser> {
+  getUsersByRole(role: string): SerializedUser[];
 }
 
 export default IUserRepository;
