e7de117f18991b42986f396d7f1f56ebf2ff511e
Milosz B <miloszbargiel@gmail.com>
2022-09-28 16:32:18 +0200

    Update ProductsController type definition

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,14 +1,22 @@
 import BaseController from './base-controller';
-import IProduct from '../interfaces/product.interface';
+import {IProduct, SerializedProduct} from '../interfaces/product.interface';
 import IProductRepository from '../repositories/products-repository';
 import { IfExists } from '../interfaces/generic-helpers';
 
-class ProductsController extends BaseController<IProduct> {
+class ProductsController extends BaseController<IProduct, SerializedProduct> {
   constructor(private productsRepository: IProductRepository) {
     super(productsRepository);
   }
 
-  getProductByName(name: string): IfExists<IProduct> {
+  validateBeforeSave(data: IProduct) {
+    return this.productsRepository.validateBeforeSave(data);
+  }
+
+  validateBeforeUpdate(data: IProduct) {
+    return this.productsRepository.validateBeforeUpdate(data);
+  }
+
+  getProductByName(name: string): IfExists<SerializedProduct> {
     return this.productsRepository.getProductByName(name);
   }
 }
