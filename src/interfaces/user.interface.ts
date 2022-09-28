59ee3f273aa0db89bf5e35711d4ecda19662d637
Milosz B <miloszbargiel@gmail.com>
2022-09-28 16:41:57 +0200

    Add SerializedUser interface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,13 +1,21 @@
 import Roles from '../enums/roles.enums';
 
-interface IUser {
+export interface IUser {
   id?: string;
   firstName: string;
   lastName: string;
   email: string;
-  dob: Date;
-  addresses?: string[];
+  dob: string;
+  addresses: string[];
   role?: Roles
 }
 
-export default IUser;
+export interface SerializedUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  dob: Date;
+  addresses: string[];
+  role: Roles
+}
